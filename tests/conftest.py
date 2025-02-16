415aeec820f566faf527554cfe8c4121a68d9cc2
rimiaziz66 <rimiaziz66@gmail.com>
2025-02-16 15:00:25 -0500

    add comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,10 @@ import json
 def my_circle():
     return shape.Circle(10)
 
+# adding path
 @pytest.fixture(scope= "module", autouse=True)
 def load_data():
     with open (r"/Users/tanvinhossaine/Desktop/pytestcode/pytest/tests/data.json", "r") as f:
         data =json.load(f)
     return data
+
